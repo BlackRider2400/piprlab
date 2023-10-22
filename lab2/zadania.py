875229e5738a2a518d8c6413e95c8a84c6965603
Krzysztof Baralkiewicz <chris@Krzysztofs-MacBook-Pro.local>
2023-10-22 22:12:24 +0200

    Setup for pycharm

--- lab2/zadania.py
+++ lab2/zadania.py
@@ -1,43 +1,49 @@
-#zadanie 1
+# zadanie 1
 def getPowerTo3(x):
     return x ** 3
 
-#zadanie 2
+
+# zadanie 2
 def getPolynomial(x, a, b, c):
     return a * x * x + b * x + c
 
-#zadanie 3
+
+# zadanie 3
 def getTimeForTask(name, number, time):
     time = time / 1000
     time = round(time, 3)
     return f"{name} wykonał(a) zadanie nr {number} w {time}s."
 
-#zadanie 4
-def getSmetric(a, b):
+
+# zadanie 4
+def getSymetric(a, b):
     c = (a[0] + b[0]) / 2
     d = (a[1] + b[1]) / 2
-    return (c, d)
+    return c, d
 
-#zadanie 5
+
+# zadanie 5
 def getBinary(x):
     return len(str(bin(x))) - 2
 
-#zadanie 6
+
+# zadanie 6
 def getAverageFromTuples(x):
     count = 0
-    sum = 0
+    sumation = 0
     for i in x:
-        sum = sum + i
+        sumation = sumation + i
         count = count + 1
     
     minimal = min(x)
     maximal = max(x)
 
-    average = sum / count
+    average = sumation / count
 
     return f"Range: < {minimal}; {maximal}> Average: {average}"
 
-#zadanie 7
+
+# zadanie 7
 def showDimensions(width, height, lenght):
     width = round(width, 3)
     height = round(height, 3)
@@ -55,18 +61,21 @@ def showDimensions(width, height, lenght):
     """
     return text
 
+
 def formatRow(a, size):
     return (size - len(a)) * " " + str(a)
 
-#zadanie 8
+
+# zadanie 8
 def getSquareEquation(a, b, c):
     delta = b*b - 4 * a * c
     if delta > 0:
         x1 = (-b - delta**0.5) / (2 * a)
         x2 = (-b + delta**0.5) / (2 * a)
-        return (x1, x2)
+        return x1, x2
     elif delta == 0:
-        return (-b / (2 * a))
+        return -b / (2 * a)
+
 
 print("---------------------------------")
 print("Zadanie 1:\n")
@@ -85,7 +94,7 @@ print(getTimeForTask("Adam", 1, 553467))
 print("---------------------------------")
 
 print("Zadanie 4:\n")
-print(getSmetric((5, 6), (8, 7)))
+print(getSymetric((5, 6), (8, 7)))
 print("---------------------------------")
 
 print("Zadanie 5:\n")
@@ -103,5 +112,3 @@ print("---------------------------------")
 print("Zadanie 8:\n")
 print(getSquareEquation(2, -4, 2))
 print("---------------------------------")
-
-
